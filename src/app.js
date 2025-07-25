7d3774169ecb7429ea7daa68219d1eba4942ceb8
Lakshmi Shetty U <lakshmishetty.61095371@ltimindtree.com>
2025-07-25 13:11:52 +0530

    playing with query params in two ways

--- src/app.js
+++ src/app.js
@@ -9,10 +9,25 @@ app.listen(port, () => {
 
 
 // Define a basic route for the homepage
+// while testing in postman it works for get and post
 app.use('/test', (req, res) => {
-    res.send('Hello World from Express!');
-  });
-  app.use('/', (req, res) => {
-    res.send('Hello World from Express Dashboard!');
-  });
+    res.send('Hello Test!');
+});
+
+//in postman works only for get
+// app.get('/user', (req, res) => {
+//   console.log(req.query);
+//   res.send({firstName: "Lakshmi", lastName: "Shetty"})
+// });
+
+app.get('/user/:userId/:paswword', (req,res)=>{
+  console.log(req.params);
+  res.send('Fetching the user with dynamic routing')
+})
+app.post('/user', (req, res) => {
+  //logic to store a user in db
+  res.send('New user data stored successfully')
+});
+
+  
 
